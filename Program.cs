4280af8173556184d883f6fcafff26aec58ef9aa
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 09:50:37 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new RankException("Сообщение об ошибке");
+            }
+            catch (RankException ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
